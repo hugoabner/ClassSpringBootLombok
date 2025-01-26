a9abf458d7fb653b670677ffdca3c8dd42dca4f4
Hugo <hugopalominogalindo974@gmail.com>
2025-01-25 23:55:56 -0500

    first commit

---
+++ src/test/java/com/todotic/project1Api/Project1ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todotic.project1Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project1ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

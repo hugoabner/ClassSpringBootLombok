eae67e3e6ed52db44c6c3daed569c2df89ead095
Hugo <hugopalominogalindo974@gmail.com>
2025-01-26 22:34:11 -0500

    create file service

--- src/main/java/com/todotic/project1Api/controller/ContactController.java
+++ src/main/java/com/todotic/project1Api/controller/ContactController.java
@@ -2,7 +2,9 @@ package com.todotic.project1Api.controller;
 
 import com.todotic.project1Api.entity.Contact;
 import com.todotic.project1Api.repository.ContactRepository;
+import com.todotic.project1Api.service.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -12,33 +14,33 @@ import java.time.LocalDateTime;
 public class ContactController {
 
     @Autowired
-    private ContactRepository contactRepository;
+    private ContactService contactService;
 
     @GetMapping
     public Iterable<Contact> list() {
-        return contactRepository.findAll();
+        return contactService.findAll();
     }
 
     @GetMapping("{id}")
     public Contact get(@PathVariable Integer id) {
-        return contactRepository
-                .findById(id)
-                .orElse(null);
+        return contactService.findById(id);
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public Contact create(@RequestBody Contact contact) {
-        contact.setCreatedAt(LocalDateTime.now());
-        return contactRepository.save(contact);
+        return contactService.create(contact);
     }
 
     @PutMapping("{id}")
     public Contact update(@PathVariable Integer id,
-                          @RequestBody Contact contact) {
-        Contact contactFromDb = contactRepository
-                .findById(id)
-                .orElse(null);
-        return contactRepository.save(contact);
+                          @RequestBody Contact form) {
+        return contactService.update(id, form);
     }
 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable Integer id) {
+        contactService.delete(id);
+    }
 }
---
+++ src/main/java/com/todotic/project1Api/service/ContactService.java
@@ -0,0 +1,48 @@
+package com.todotic.project1Api.service;
+
+import com.todotic.project1Api.entity.Contact;
+import com.todotic.project1Api.repository.ContactRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.time.LocalDateTime;
+
+@Service
+public class ContactService {
+    @Autowired
+    private ContactRepository contactRepository;
+
+    public Iterable<Contact> findAll() {
+        return contactRepository.findAll();
+    }
+
+    public Contact findById(Integer id) {
+        return contactRepository
+                .findById(id)
+                .orElse(null);
+    }
+
+    public Contact create(Contact contact) {
+        contact.setCreatedAt(LocalDateTime.now());
+        return contactRepository.save(contact);
+    }
+
+    public Contact update(Integer id, Contact form) {
+        Contact contactFromDb = contactRepository
+                .findById(id)
+                .orElse(null);
+        contactFromDb.setName(form.getName());
+        contactFromDb.setEmail(form.getEmail());
+        return contactRepository.save(contactFromDb);
+    }
+
+    public void delete(Integer id) {
+        Contact contactFromDb = contactRepository
+                .findById(id)
+                .orElse(null);
+        contactRepository.delete(contactFromDb);
+    }
+
+}

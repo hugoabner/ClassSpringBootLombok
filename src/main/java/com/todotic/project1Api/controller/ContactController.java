7fffc2763950905c9549fbea808ac2da967d1f01
Hugo <hugopalominogalindo974@gmail.com>
2025-01-26 21:00:13 -0500

    endpoint para crear un contacto

--- src/main/java/com/todotic/project1Api/controller/ContactController.java
+++ src/main/java/com/todotic/project1Api/controller/ContactController.java
@@ -3,10 +3,7 @@ package com.todotic.project1Api.controller;
 import com.todotic.project1Api.entity.Contact;
 import com.todotic.project1Api.repository.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api/contacts")
 @RestController
@@ -22,7 +19,14 @@ public class ContactController {
 
     @GetMapping("{id}")
     public Contact get(@PathVariable Integer id) {
-        return contactRepository.findById(id).orElse(null);
+        return contactRepository
+                .findById(id)
+                .orElse(null);
+    }
+
+    @PostMapping("")
+    public Contact create(Contact contact) {
+        return contactRepository.save(contact);
     }
 
 }
